947826ec97e21274b8ddf07dff3ae03b4ca33c13
Roberto Pozo <roberto.pozo.andrade@gmail.com>
2017-12-26 19:33:11 -0500

    Add files via upload
    Code for know if a car is free to go on road.

---
+++ Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Pico_Placa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_plate = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_date = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_time = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbl_answ = new System.Windows.Forms.Label();
+            this.btn_go = new System.Windows.Forms.Button();
+            this.err_1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.err_1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(150, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pico y Placa Predictor";
+            // 
+            // txt_plate
+            // 
+            this.txt_plate.Location = new System.Drawing.Point(233, 81);
+            this.txt_plate.Name = "txt_plate";
+            this.txt_plate.Size = new System.Drawing.Size(100, 20);
+            this.txt_plate.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "License Plate Number (LLL-NNNN)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Date (dd-mm-yyyy)";
+            // 
+            // txt_date
+            // 
+            this.txt_date.Location = new System.Drawing.Point(233, 125);
+            this.txt_date.Name = "txt_date";
+            this.txt_date.Size = new System.Drawing.Size(100, 20);
+            this.txt_date.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Time (hh:mm:ss)";
+            // 
+            // txt_time
+            // 
+            this.txt_time.Location = new System.Drawing.Point(232, 168);
+            this.txt_time.Name = "txt_time";
+            this.txt_time.Size = new System.Drawing.Size(100, 20);
+            this.txt_time.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(44, 212);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Answer: ";
+            // 
+            // lbl_answ
+            // 
+            this.lbl_answ.AutoSize = true;
+            this.lbl_answ.Location = new System.Drawing.Point(98, 212);
+            this.lbl_answ.Name = "lbl_answ";
+            this.lbl_answ.Size = new System.Drawing.Size(0, 13);
+            this.lbl_answ.TabIndex = 8;
+            // 
+            // btn_go
+            // 
+            this.btn_go.Location = new System.Drawing.Point(355, 168);
+            this.btn_go.Name = "btn_go";
+            this.btn_go.Size = new System.Drawing.Size(75, 23);
+            this.btn_go.TabIndex = 9;
+            this.btn_go.Text = "Go";
+            this.btn_go.UseVisualStyleBackColor = true;
+            this.btn_go.Click += new System.EventHandler(this.btn_go_Click);
+            // 
+            // err_1
+            // 
+            this.err_1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 268);
+            this.Controls.Add(this.btn_go);
+            this.Controls.Add(this.lbl_answ);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt_time);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_date);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_plate);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.err_1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_plate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_date;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_time;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbl_answ;
+        private System.Windows.Forms.Button btn_go;
+        private System.Windows.Forms.ErrorProvider err_1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace Pico_Placa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_go_Click(object sender, EventArgs e)
+        {
+            if (Is_Valide())
+            {
+                bool p_y_p_date = false, p_y_p_time = false;
+                string plate = txt_plate.Text;
+                string s_number = string.Empty;
+                byte b_number;
+
+                foreach (char c in plate)
+                {
+                    s_number = c.ToString();
+                }
+
+
+                if (Byte.TryParse(s_number, out b_number))
+                {
+                    DateTime d_date = DateTime.Parse(txt_date.Text);
+                    DayOfWeek dow = d_date.DayOfWeek;
+                   
+                    switch (b_number)
+                    {
+                        case 1:
+                        case 2:
+                            if (dow == DayOfWeek.Monday)
+                                p_y_p_date = true;
+                            break;
+
+                        case 3:
+                        case 4:
+                            if (dow == DayOfWeek.Tuesday)
+                                p_y_p_date = true;
+                            break;
+
+                        case 5:
+                        case 6:
+                            if (dow == DayOfWeek.Wednesday)
+                                p_y_p_date = true;
+                            break;
+
+                        case 7:
+                        case 8:
+                            if (dow == DayOfWeek.Thursday)
+                                p_y_p_date = true;
+                            break;
+
+                        case 9:
+                        case 0:
+                            if (dow == DayOfWeek.Friday)
+                                p_y_p_date = true;
+                            break;
+
+                    }
+
+                   
+
+                    TimeSpan t_time = TimeSpan.Parse(txt_time.Text);
+                    TimeSpan t_start1 = new TimeSpan(7, 0, 0);
+                    TimeSpan t_end1 = new TimeSpan(9, 30, 0);
+                    TimeSpan t_start2 = new TimeSpan(16, 0, 0);
+                    TimeSpan t_end2 = new TimeSpan(19, 30, 0);
+                    if (t_time >= t_start1 && t_time <= t_end1)
+                    {
+                        p_y_p_time = true;
+                    }
+                    else if (t_time >= t_start2 && t_time <= t_end2)
+                    {
+                        p_y_p_time = true;
+                    }
+
+                    if (p_y_p_date && p_y_p_time)                    
+                        lbl_answ.Text = "The car with this plate number can't go on road";                    
+                    else
+                        lbl_answ.Text = "The car with this plate number can go on road. Drive safe!";                    
+                }
+                else
+                    MessageBox.Show("The plate number is incorrect, please check the format", "Error");
+            }
+        }
+        private bool Is_Valide()
+        {
+            bool ok = true;
+
+            err_1.Clear();
+            var dateFormats = new[] { "dd.MM.yyyy", "dd-MM-yyyy", "dd/MM/yyyy" };
+
+            DateTime date;
+            bool validDate = DateTime.TryParseExact(
+                txt_date.Text,
+                dateFormats,
+                DateTimeFormatInfo.InvariantInfo,
+                DateTimeStyles.None,
+                out date);
+
+            TimeSpan time;
+
+            if (string.IsNullOrEmpty(txt_plate.Text))
+            {
+
+                err_1.SetError(txt_plate, "You must enter a plate number");
+                ok = false;
+            }
+            else if (txt_plate.Text.Length < 6)
+            {
+                err_1.SetError(txt_plate, "The plate number must contain at least 6 characters");
+                ok = false;
+            }
+
+
+            if (string.IsNullOrEmpty(txt_date.Text))
+            {
+                err_1.SetError(txt_date, "Please enter a valid date");
+                ok = false;
+            }
+            else if (!validDate)
+            {
+                err_1.SetError(txt_date, "The date must be in the order day month year");
+                ok = false;
+            }
+
+
+            if (string.IsNullOrEmpty(txt_time.Text))
+            {
+                err_1.SetError(txt_time, "Please enter a valid time");
+                ok = false;
+            }
+            else
+            {
+                try
+                {
+                    time = TimeSpan.Parse(txt_time.Text);
+                }
+                catch (FormatException)
+                {
+                    err_1.SetError(txt_time, "The time is incorrect");
+                    ok = false;
+                }
+
+
+            }
+
+            return ok;
+        }
+    }
+}
